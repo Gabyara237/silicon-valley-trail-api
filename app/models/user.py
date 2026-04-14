74f6d9e10e324668d3b2dea856413012f590114d
Gabriela Araujo <gabyara237@gmail.com>
2026-04-14 01:15:37 -0700

    feat: add user model

---
+++ app/models/user.py
@@ -0,0 +1,16 @@
+from datetime import datetime, timezone
+from typing import Optional
+from sqlmodel import SQLModel,Field
+
+
+
+class User(SQLModel, table=True):
+
+    __tablename__= "users"
+    
+    id: Optional[int] = Field(default=None,primary_key=True)
+    username: str = Field(max_length=50, index=True, unique=True, nullable=False)
+    email: str = Field(max_length=255, index=True, unique=True, nullable=False)
+    hashed_password: str = Field(max_length=255, nullable=False)
+    created_at: datetime = Field(default_factory= lambda:datetime.now(timezone.utc))
+
