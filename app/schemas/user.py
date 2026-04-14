09262ced5676631af719ab243f67a3b867e47091
Gabriela Araujo <gabyara237@gmail.com>
2026-04-14 16:10:54 -0700

    feat: add user schemas

---
+++ app/schemas/user.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, EmailStr, Field
+
+class UserCreate(BaseModel):
+    username: str = Field(min_length=3, max_length=50)
+    password: str = Field(min_length=6, max_length=100)
+    email: EmailStr 
+ 
+    
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+    
+    class Config: 
+        from_attributes= True
+
+
